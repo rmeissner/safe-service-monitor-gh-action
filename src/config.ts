089f9a42490cb90c4740586a8f55f5e15bb19316
Richard Meissner <msc.meissner@gmail.com>
2021-12-05 23:12:11 +0100

    Use PAT

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 import { getOctokit } from "@actions/github";
 
 export function githubToken(): string {
-    const token = process.env.GITHUB_TOKEN;
+    let token = process.env.PERSONAL_ACCESS_TOKEN;
+    if (!token)
+        token = process.env.GITHUB_TOKEN;
     if (!token)
         throw ReferenceError('No token defined in the environment variables');
     return token;
