4d600db0b421e92955108d3c690c8380e907449c
Richard Meissner <msc.meissner@gmail.com>
2021-12-10 17:59:55 +0100

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,13 @@ async function run(): Promise<void> {
   try {
     const safeAddress: string = core.getInput('safe-address')
     const serviceUrl: string = core.getInput('service-url')
-    const workflowTrigger: string = core.getInput('workflow-trigger')
     const infoResponse: AxiosResponse<SafeInfo> = await axios.get(`${serviceUrl}/api/v1/safes/${safeAddress}`)
     const safeInfo = infoResponse.data
     console.log("Safe Information", safeInfo)
 
     const txResponse: AxiosResponse<Page<MultisigTransaction>> = await axios.get(`${serviceUrl}/api/v1/safes/${safeAddress}/multisig-transactions/?nonce__gte=${safeInfo.nonce}`)
     for (const transaction of txResponse.data.results) {
-      console.log("Check transaction", transaction)
+      console.log("Sync transaction", transaction)
       const trigger: MultiSigTrigger = {
         type: 'multisig',
         id: transaction.safeTxHash
